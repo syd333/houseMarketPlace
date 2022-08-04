bea7323e834698d439ef204444941ed03face326
syd333 <sydmiranda@icloud.com>
2022-08-04 13:29:17 -0400

    dummy routes

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,10 +1,25 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
-    <div>
-      house marketplace
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Signin />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
