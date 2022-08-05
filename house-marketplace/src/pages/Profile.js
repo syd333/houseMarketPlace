5139d41744279a35054ce528848dc8726b89f423
syd333 <sydmiranda@icloud.com>
2022-08-05 13:58:55 -0400

    profile + update profile details

--- house-marketplace/src/pages/Profile.js
+++ house-marketplace/src/pages/Profile.js
@@ -21,7 +21,7 @@ function Profile() {
   const auth = getAuth();
 
   const [loading, setLoading] = useState(true);
-
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -30,6 +30,29 @@ function Profile() {
   const { name, email } = formData;
 
   const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        const userRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+    } catch (error) {
+      toast.error("Could not update profile details");
+    }
+  };
 
   const onLogout = () => {
     auth.signOut();
@@ -47,6 +70,35 @@ function Profile() {
       <main>
         <div className="profileDetailsHeader">
           <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
         </div>
       </main>
     </div>
