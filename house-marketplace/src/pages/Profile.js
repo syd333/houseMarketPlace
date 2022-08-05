d0e10aac32453a7bb4259dde16d2b06852d2e2fd
syd333 <sydmiranda@icloud.com>
2022-08-05 13:36:29 -0400

    logout

--- house-marketplace/src/pages/Profile.js
+++ house-marketplace/src/pages/Profile.js
@@ -1,15 +1,56 @@
 import React, { useState, useEffect } from "react";
-import { getAuth } from "firebase/auth";
+import { Link } from "react-router-dom";
+import { getAuth, updateProfile } from "firebase/auth";
+import {
+  updateDoc,
+  doc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
+import homeIcon from "../assets/svg/homeIcon.svg";
 
 function Profile() {
   const auth = getAuth();
 
-  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
-  return user ? <div>hey {user.displayName}</div> : 'not logged in';
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Profile;
