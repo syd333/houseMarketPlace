5e2f293996d4b98ee3586913a0635a1083386076
syd333 <sydmiranda@icloud.com>
2022-08-11 15:53:09 -0400

    swiper error

--- house-marketplace/src/pages/Listing.js
+++ house-marketplace/src/pages/Listing.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
 import Spinner from "../components/Spinner";
 import shareIcon from "../assets/svg/shareIcon.svg";
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 function Listing() {
   const [listing, setListing] = useState(null);
@@ -35,6 +40,22 @@ function Listing() {
 
   return (
     <main>
+      {/* <Helmet> */}
+        <title>{listing.name}</title>
+      {/* </Helmet> */}
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: "cover",
+              }}
+              className="swiperSlideDiv"
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <div
         className="shareIconDiv"
         onClick={() => {
@@ -89,7 +110,7 @@ function Listing() {
         <p className="listingLocationTitle">Location</p>
 
         <div className="leafletContainer">
-          {/* <MapContainer
+          <MapContainer
             style={{ height: "100%", width: "100%" }}
             center={[listing.geolocation.lat, listing.geolocation.lng]}
             zoom={13}
@@ -105,7 +126,7 @@ function Listing() {
             >
               <Popup>{listing.location}</Popup>
             </Marker>
-          </MapContainer> */}
+          </MapContainer>
         </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
